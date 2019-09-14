c2214e629079c2ff8232243feb5fbe9f09234968
Pablo Nevares <pablo@nevares.com>
2019-09-14 15:12:01 -0700

    Covered test cases

--- mini-jump.js
+++ mini-jump.js
@@ -8,22 +8,26 @@
 
 const miniJump = steps => {
   let skipToPosition;
-
+  
   return steps.reduce((totalJumps, maxJumps, currentPosition) => {
     if (currentPosition === steps.length - 1) return totalJumps;
     if (skipToPosition && skipToPosition !== currentPosition) return totalJumps;
+    if (currentPosition + maxJumps >= steps.length - 1) {
+      skipToPosition = steps.length - 1;
+      return totalJumps + 1;
+    }
     if (maxJumps === 0) return -1;
 
-    // skipToPosition =
-    //   currentPosition +
-    //   steps
-    //     .slice(currentPosition + 1, currentPosition + 1 + maxJumps)
-    //     .reduce((bestIndex, currentJumps, currentIndex, options) => {
-    //       if (options[currentIndex >= options[bestIndex]]) return currentIndex;
-    //       return bestIndex;
-    //     }, 0);
+    skipToPosition =
+      currentPosition + 1 +
+      steps
+        .slice(currentPosition + 1, currentPosition + 1 + maxJumps)
+        .reduce((bestIndex, currentJumps, currentIndex, options) => {
+          if (!bestIndex || options[currentIndex] >= options[bestIndex]) return currentIndex;
+          return bestIndex;
+        }, null);
 
-    return (totalJumps += 1);
+    return totalJumps + 1;
   }, 0);
 };
 
